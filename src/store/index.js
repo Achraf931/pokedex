b44d6986e2386ae5675c8223fb937e736dfb3936
Achraf <hamrouni.pro@outlook.fr>
2022-04-09 16:53:46 +0200

    init base store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import { createStore } from 'vuex'
 
+const mutationList = {
+  isLoggedIn: 'IS_LOGGED_IN'
+}
+
+export const actionList = {
+  detectLoggedIn: 'DETECT_LOGGED_IN'
+}
+
 export default createStore({
   state: {
+    loggedIn: localStorage.getItem('refresh_token') !== null
   },
   getters: {
   },
   mutations: {
+    [mutationList.isLoggedIn] (state, bool) {
+      state.loggedIn = bool
+    }
   },
   actions: {
+    [actionList.detectLoggedIn] ({ commit }) {
+      commit(mutationList.isLoggedIn, localStorage.getItem('refresh_token') !== null)
+    }
   },
   modules: {
   }
