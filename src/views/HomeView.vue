16cf725810d628746375619c904fdf990d245bbe
Achraf <hamrouni.pro@outlook.fr>
2022-04-09 16:04:18 +0200

    init v2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,6 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import HelloWorld from '@/components/HelloWorld'
 </script>
