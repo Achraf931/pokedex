2622d81ed238c295d7cfe00e3d7814c933c7cf4b
Achraf <hamrouni.pro@outlook.fr>
2022-04-09 17:17:06 +0200

    add all service methods

--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,31 @@ import axios from 'axios'
 
 export const baseURL = process.env.VUE_APP_API
 export const handler = axios.create({ baseURL })
+
+export const get = async (endpoint = '', data) => {
+  if (typeof data !== 'undefined') {
+    endpoint = `${endpoint.replace(/\?.*$/, '')}?${new URLSearchParams(data)}`
+  }
+
+  return await handler.get(endpoint)
+}
+
+export const post = async (endpoint = '', data) => {
+  await handler.post(endpoint, data)
+}
+
+export const put = async (endpoint = '', data) => {
+  await handler.put(endpoint, data)
+}
+
+export const patch = async (endpoint = '', data) => {
+  await handler.patch(endpoint, data)
+}
+
+export const del = async (endpoint = '', data) => {
+  if (typeof data !== 'undefined') {
+    endpoint = `${endpoint.replace(/\?.*$/, '')}?${new URLSearchParams(data)}`
+  }
+
+  return await handler.delete(endpoint)
+}
